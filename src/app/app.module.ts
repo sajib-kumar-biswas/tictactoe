782ee3a8e96da784a0677bf32c48d99bf7dc3b59
sajib-kumar-biswas <sajibbiswas01521457841@gmail.com>
2023-03-28 18:52:18 +0600

    added level1 component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Level1Component } from './level1/level1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Level1Component
   ],
   imports: [
     BrowserModule
